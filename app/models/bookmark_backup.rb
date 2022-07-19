260e297b64bf19331321362acce861b4e67503fa
Menakadevi Sundaramoorthy <menakadevisundaramoorthy@gmail.com>
2022-07-19 21:27:45 +0200

    updated

--- app/models/bookmark_backup.rb
+++
@@ -1,6 +0,0 @@
-class Bookmark < ApplicationRecord
-  belongs_to :movie
-  belongs_to :list
-  validates :movies_id, uniqueness: { scope: :lists_id }
-  validates :comment, length: { minimum: 6 }
-end
